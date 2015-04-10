57a798b98dec20683daef627237f37ce44b16779
Kirill Kouznetsov <agon.smith@gmail.com>
2015-04-10 23:19:16 +0300

    update support section of this cookbook

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,12 @@ description 'Configure the system timezone on Debian or Ubuntu.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.4'
 
-%w( ubuntu debian ).each do |os|
+%w(
+  centos
+  debian
+  fedora
+  rhel
+  ubuntu
+).each do |os|
   supports os
 end
